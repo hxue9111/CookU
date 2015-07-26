f407959cf9237b213d0c96333eb7cb99ee86fe02
Julie Muzy <julie.m.muzy@gmail.com>
2015-07-26 13:19:12 -0500

    Have basic layout for RecipleDetailsFragment, worked a little on RecipeListFragment, but having issues hard coding things to make sure it works

--- app/src/main/java/com/example/cooku/cooku/MainActivity.java
+++ app/src/main/java/com/example/cooku/cooku/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.cooku.cooku;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,7 +11,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);  /* Change this to see a certain view on app load */
     }
 
     @Override
@@ -35,4 +35,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
