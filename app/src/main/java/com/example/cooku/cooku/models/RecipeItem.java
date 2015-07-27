c83cdb40d24b9d6b97dd2cdd6c6e1c77f354c08f
Huang Xue <huangxue9111@gmail.com>
2015-07-27 19:32:03 -0400

    added image_url, renamed vars

--- app/src/main/java/com/example/cooku/cooku/models/RecipeItem.java
+++ app/src/main/java/com/example/cooku/cooku/models/RecipeItem.java
@@ -4,21 +4,26 @@ package com.example.cooku.cooku.models;
  * Created by Huang Xue on 7/27/2015.
  */
 public class RecipeItem {
-    private String name, URL, id;
-    public RecipeItem(String name, String URL, String id) {
+    private String name, image_url, source_url, recipe_id;
+    public RecipeItem(String name, String image_url,String source_url, String recipe_id) {
         this.name = name;
-        this.URL = URL;
-        this.id = id;
+        this.image_url = image_url;
+        this.source_url = source_url;
+        this.recipe_id = recipe_id;
     }
 
     public String getName(){
         return name;
     }
-    public String getURL(){
-        return URL;
+    public String getImageUrl(){
+        return image_url;
     }
 
-    public String getId(){
-        return id;
+    public String getSourceUrl(){
+        return source_url;
+    }
+
+    public String getRecipeId(){
+        return recipe_id;
     }
 }
