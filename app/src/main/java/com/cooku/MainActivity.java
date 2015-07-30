af09e190d663e63e82bf807192e356490db7fa32
Huang Xue <huangxue9111@gmail.com>
2015-07-29 20:21:29 -0400

    mainactivity

--- app/src/main/java/com/cooku/MainActivity.java
+++ app/src/main/java/com/cooku/MainActivity.java
@@ -58,6 +58,16 @@ public class MainActivity extends ActionBarActivity
         transaction.addToBackStack(null);
         transaction.commit();
     }
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 0 ){
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
     public void onFragmentInteraction(Uri uri){
     }
 }
